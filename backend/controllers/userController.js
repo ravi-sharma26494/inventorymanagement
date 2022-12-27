dfb72de09d3880175cd2c000bf3a0976693ad12e
Ravi Sharma <ravi.sharma26494@gmail.com>
2022-12-27 23:20:33 +0530

    UserController-register user basic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,41 @@
-const registerUser =  (req, res)=>{
-    if(!req.body.email){
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel');
+
+const registerUser =  asyncHandler( async(req, res)=>{
+    const {name, email, password} = req.body;
+    
+    //validation
+    if(!name || !email || !password){
         res.status(400)
-        throw new Error("Please add an email")
+        throw new Error("Please fill in all the required fields.");
     }
-    res.send('register the user');
-}
+    if(password.length <6){
+        res.status(400)
+        throw new Error("Password must have atleast 6 Characters");
+    }
+
+    //check if user already exist
+    const usersExists = await User.findOne({email});
+    if(usersExists){
+        res.status(404)
+        throw new Error("Email has already been registered.")
+
+    }
+    //Create a new User
+    const user = await User.create({
+        name, email, password
+    })
+    if(user){
+        const{_id, name, email, photo, phone, bio} = user;
+        res.status(201).json({
+            _id, name, email, photo, phone, bio
+        })
+    } else{
+        res.status(400)
+        throw new Error("Invalid User Data")
+    }
+
+});
 
 module.exports = {
     registerUser
