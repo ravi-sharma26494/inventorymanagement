21e3e48c683ee3790e77736d1912135fb295d451
Ravi Sharma <ravi.sharma26494@gmail.com>
2022-12-28 13:02:25 +0530

    protectmiddleware & getting userdata getUser route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,7 +129,21 @@ const logout = asyncHandler(async(req, res)=>{
 
 // Get USER pROFILE DATA //
 const getUser = asyncHandler(async(req, res)=>{
-    res.send
+    const user = await User.findById(req.user._id);
+    if(user){
+        const{_id, name, email, photo, phone, bio} = user;
+        res.status(200).json({
+            _id, 
+            name, 
+            email, 
+            photo, 
+            phone, 
+            bio,
+        })
+    } else{
+        res.status(400)
+        throw new Error("User Not Found")
+    }
 });
 
 module.exports = {
