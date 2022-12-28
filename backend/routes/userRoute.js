46a0dcd524aaec7d81d412cea1ee0b19e9e1f1e6
Ravi Sharma <ravi.sharma26494@gmail.com>
2022-12-28 12:48:49 +0530

    protect middleware for routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, logout, getUser} = require('../controllers/userController')
+const {registerUser, loginUser, logout, getUser} = require('../controllers/userController');
+const { protect } = require('../middleWares/authMiddleware');
 
 
 router.post('/register',registerUser)
 router.post('/login',loginUser);
 router.get('/logout',logout);
-router.get('/getuser',getUser);
+router.get('/getuser', protect, getUser);
 
 module.exports = router;
