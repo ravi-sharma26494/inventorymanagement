650550b4ef257db6278f1742c035666d82fe5edc
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-01 15:08:13 +0530

    auth components only

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Forgot from './pages/auth/Forgot';
+import Reset from './pages/auth/Reset';
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/forgot' element={<Forgot />}/>
+        <Route path='/resetpassword/:resetToken' element={<Reset />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
