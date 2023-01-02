82f20a8ee39eaf2de5c8d1b9773707ce62d83127
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-03 00:12:17 +0530

    cors error handling added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000, https://rinvent.vercel.app"],
+    credentials:true
+}));
 
 //Routes Middlewares
 app.use("/api/users", userRoute)
