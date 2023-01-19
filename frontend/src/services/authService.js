947cf92ddb6b8def00ec06f22063365a660faac8
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-19 21:11:30 +0530

    Setting up service to login the user

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,7 +5,8 @@ export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 
 export const validateEmail = (email)=>{
     return email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-}
+};
+
 export const registerUser= async (userData)=>{
     try {
         
@@ -22,4 +23,23 @@ export const registerUser= async (userData)=>{
         ) || error.message || error.toString();
         toast.error(message)
     }
-}
+};
+
+//Login User
+export const loginUser= async (userData)=>{
+    try {
+        
+        const response = await axios.post(`${BACKEND_URL}/api/users/login`, userData); 
+        
+
+        if(response.statusText === "OK"){
+            toast.success("Login Successfull")
+        }
+        return response.data;
+    } catch (error) {
+        const message = (
+            error.response && error.response.data  && error.response.data.message
+        ) || error.message || error.toString();
+        toast.error(message)
+    }
+};
