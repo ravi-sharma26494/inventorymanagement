13e6094ea078593c49c900824caac3318b0a7549
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-20 18:40:54 +0530

    file upload uitility function created

---
+++ backend/utils/fileUpload.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+
+// define file storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, Date().toISOString().replace(/:/g, "-") + file.originalname)
+    }
+  });
+
+  //specify file format that can be saved
+  function fileFilter (req, file, cb) {
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ){
+        cb(null, true)
+    } else{
+
+        cb(null, false)
+    }
+    
+  
+  }
+
+
+  const upload = multer({ storage , fileFilter});
+  module.exports = {upload};
+  
