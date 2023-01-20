fe51d29328561d5115bce5263940d6f5fb75804a
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-20 18:11:45 +0530

    implemented prod cntrller, created sample in db

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,29 @@
 const asyncHandler = require('express-async-handler');
+const Product = require('../models/productModel');
 
 const createProduct = asyncHandler (async (req,res)=>{
-    res.send("product created")
+    const {name, sku, category, quantity, price, description} = req.body;
+
+    //Validation
+    if(!name || !category || !quantity || !price || !description){
+        res.status(400);
+        throw new Error("Please add all the fields");
+    }
+
+    //Manage Image upload
+    //create product
+    const product = await Product.create({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description
+    });
+
+    res.status(201).json(product);
+
 });
 
 module.exports = {
