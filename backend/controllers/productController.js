85cc6baf2d6386cd06274514dc52de70f64da517
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-21 11:51:45 +0530

    cloudinary upload implemented

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 const { fileSizeFormatter } = require('../utils/fileUpload');
+const cloudinary = require('cloudinary').v2;
 
 const createProduct = asyncHandler (async (req,res)=>{
     const {name, sku, category, quantity, price, description} = req.body;
@@ -14,9 +15,18 @@ const createProduct = asyncHandler (async (req,res)=>{
     //Handle Image upload
     let fileData = {};
     if(req.file){
+        //saving the image in cloudinary
+        let uploadedFile;
+        try {
+            uploadedFile =  await cloudinary.uploader.upload(req.file.path, {folder: "Pinvent App", resource_type: "image"})
+        } catch (error) {
+            res.status(500);
+            throw new Error("Image could not be upload, please try again");
+        }
         fileData = {
             fileName: req.file.originalname,
-            filePath: req.file.path,
+            // filePath: req.file.path,
+            filePath: uploadedFile.secure_url,
             fileType: req.file.mimetype,
             fileSize: fileSizeFormatter(req.file.size, 2),
         }
