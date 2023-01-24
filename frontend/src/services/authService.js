dd2de8ef6ce23c7e43960611605cb35df7248526
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-24 21:07:02 +0530

    Getting the login status of the user

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -87,3 +87,18 @@ export const resetPassword= async (userData, resetToken)=>{
         toast.error(message)
     }
 };
+
+// get login status
+export const getLoginStatus= async ()=>{
+    try {
+        
+        const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`);
+        return response.data; 
+        
+    } catch (error) {
+        const message = (
+            error.response && error.response.data  && error.response.data.message
+        ) || error.message || error.toString();
+        toast.error(message)
+    }
+};
