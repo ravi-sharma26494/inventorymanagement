2ed3856ad3369a6eb7c2116ac57004003a249d1e
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-02-04 23:51:17 +0530

    Revert "revert back to same db connection"
    This reverts commit c383535d1e6f1ca82cace6b3db40d0ea54af0d8c.

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,13 @@ app.use(errorHandler);
 
 //Connection to database and start server
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
-        .then(console.log("DB Connection Suscessfull"))
-        .catch((err)=> console.log(err))
+const dbconnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("DB Connection Suscessfull");
+    } catch (err) {
+        console.log(err);
+    }
+}
+dbconnection();    
 app.listen(PORT, ()=> console.log(`Server is running at: ${PORT}`))
