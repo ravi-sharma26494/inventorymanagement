030ec26793c8cd4982a02de9b9b9baa7cb1dca01
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-02-06 17:47:00 +0530

    Profile component jsx logic implemented

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
+import { SpinnerImg } from '../../components/loader/Loader';
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 import { SET_NAME, SET_USER } from '../../redux/features/auth/authSlice';
 import { getUser } from '../../services/authService';
@@ -27,8 +30,38 @@ const Profile = () => {
   
 
   return (
-    <div>
-
+    <div className='profile --my2'>
+      {isLoading && <SpinnerImg />}
+      <>
+        {!isLoading && profile === null ? (
+          <p>Something went wrong ,please reload the page...</p>
+        ) : (
+          <Card cardClass={"card --flex-dir-column"}>
+            <span className="profile-photo">
+              <img src={profile?.photo} alt="profilepic" />
+            </span>
+            <span className="profile-data">
+              <p>
+                <b>Name : </b> {profile?.name}
+              </p>
+              <p>
+                <b>Email : </b> {profile?.email}
+              </p>
+              <p>
+                <b>Phone : </b> {profile?.phone}
+              </p>
+              <p>
+                <b>Bio : </b> {profile?.bio}
+              </p>
+              <div>
+                <Link to="/edit-profile">
+                  <button className="--btn --btn-primary">Edit Profile</button>
+                </Link>
+              </div>
+            </span>
+          </Card>
+        )}
+      </>
     </div>
   )
 }
