6a2001e9c24ef7f81d9fb339fec43ebe2b463a01
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-02-06 17:17:53 +0530

    get user function > authservice

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -102,3 +102,18 @@ export const getLoginStatus= async ()=>{
         toast.error(message)
     }
 };
+
+// get user profile
+export const getUser= async ()=>{
+    try {
+        
+        const response = await axios.get(`${BACKEND_URL}/api/users/getuser`);
+        return response.data; 
+        
+    } catch (error) {
+        const message = (
+            error.response && error.response.data  && error.response.data.message
+        ) || error.message || error.toString();
+        toast.error(message)
+    }
+};
