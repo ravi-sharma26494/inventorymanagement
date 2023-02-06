1fe7c1191fb05c2510faaf6d002059e52c18930c
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-02-06 17:39:17 +0530

    profile component gettin user data implemented

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
+import { SET_NAME, SET_USER } from '../../redux/features/auth/authSlice';
+import { getUser } from '../../services/authService';
 import "./Profile.scss";
 
+
 const Profile = () => {
+  useRedirectLoggedOutUser("/login");
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setIsLoading(true);
+    async function getUserData(){
+      const data = await getUser();
+      console.log(data);
+
+      setProfile(data);
+      setIsLoading(false);
+      await dispatch(SET_USER(data));
+      await dispatch(SET_NAME(data.name));
+    }
+    getUserData();
+  }, [dispatch]);
+  
+
   return (
-    <div>Profile</div>
+    <div>
+
+    </div>
   )
 }
 
